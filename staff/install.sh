0cbd2f98d473c301c6c35e155a626d0462232f4f
azazin <30638197+azazin@users.noreply.github.com>
2018-11-23 14:03:30 +0200

    ssh works fine
    local repo  addded

--- staff/install.sh
+++ staff/install.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-USER="ansible"
+NEWUSER=ansible
+NEWUSERPASS=ansible
+
 rm /etc/hosts
 cp /vagrant/hosts /etc/hosts
 tar -xzvf /vagrant/yum.repos.d.tar.gz -C /etc/
 yum install epel-release -y
 yum update -y
-yum install -y mc htop iftop iotop elinks wget vim mlocate  lsof git #python-setuptools
-#easy_install pip
-#pip install --upgrade pip
-#pip install ansible
-#mv /etc/localtime /etc/localtime_old
-#ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
+yum install -y mc htop iftop iotop elinks wget vim mlocate   lsof git #python-setuptools
 timedatectl set-timezone Europe/Kiev
 
-    useradd -m -p $(openssl passwd $USER) $USER
-    usermod -aG wheel $USER
-    echo "$USER         ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-	
-	install -d /home/$USER/.ssh -g $USER -o $USER -m 700 
-	ssh-keygen  -f /home/$USER/.ssh/id_rsa -C $USER"@"$HOSTNAME
-	chown -R $USER:  /home/$USER/.ssh/*
-	
+    useradd -m -p $(openssl passwd  $NEWUSERPASS) $NEWUSER
+    usermod -aG wheel $NEWUSER
+    echo "$NEWUSER         ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+	install -d /home/$NEWUSER/.ssh -g $NEWUSER -o $NEWUSER -m 700 
+	ssh-keygen  -f /home/$NEWUSER/.ssh/id_rsa -C $NEWUSER"@"$HOSTNAME
+	chown -R $NEWUSER:  /home/$NEWUSER/.ssh/*
 sed /etc/ssh/sshd_config -e 's/PasswordAuthentication no/PasswordAuthentication yes/' -i
 systemctl  restart sshd
+updatedb
